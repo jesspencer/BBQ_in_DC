4bc7b9d045e3da7023203171fe28ca2967ba8511
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-27 11:59:46 -0400

    Added star rating column to restaurant datbase.py file

--- database_setup.py
+++ database_setup.py
@@ -13,6 +13,7 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    stars = Column(String(5), nullable=False)
 
 
 class MenuItem(Base):
