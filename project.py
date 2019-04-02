1afc90b57a2c82d97c8e8c30d91d726b1f570860
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-04-02 14:56:22 -0400

    ADDED 'ASC' TO THE IMPORTS IN PROJECT.PY IN ORDER TO USE THE COMMAND WHEN LISTING ALL RESTAURANTS

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc 
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
