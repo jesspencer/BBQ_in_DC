e6533c06a86865186cc756851bdb6a257db5cf47
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-04-02 14:51:28 -0400

    ADDED FUNCTIONALITY TO DISPLAY ALL RESTATURANTS Updated the menuIndex.html template so that it could render the restaurant in database

--- finalproject.py
+++ finalproject.py
@@ -1,11 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-
 def hello_world():
     return "hello"
-
 
 
 
--- project.py
+++ project.py
@@ -17,12 +17,17 @@ def restaurantMenuJSON(restaurant_id):
     items = session.query(MenuItem).filter_by(id=restaurant_id).all()
     return jsonify(MenuItems = [i.serialize for i in items])
 
-@app.route('/restaurants/<int:restaurant_id>mneu/<int:menu_id>/JSON')
+@app.route('/restaurants/<int:restaurant_id>menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
     menuItem=session.query(MenuItem).filter_by(id=menu_id).one()
     return jsonify(MenuItem = menuItem.serialize)
 
 @app.route('/')
+@app.route('/restaurants/')
+def showRestaurant():
+    restaurants = sessioin.query(Restaurant).order_by(asc(Restaurant.name))
+    return render_template('menuIndex.html', restaurants = restaurants)
+    
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
